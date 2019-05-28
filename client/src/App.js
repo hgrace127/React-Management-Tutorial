1ea1a48376e045cd55ef6d51a8d09f23b08a77ea
KimSeungju <21300127@handong.edu>
2019-05-28 11:46:25 +0900

    Add Node.js Express Server Modules

--- client/src/App.js
+++ client/src/App.js
@@ -67,9 +67,12 @@ class App extends Component{
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} gender={c.gender} birthday={c.birthday} job={c.job}/>)})}
+        {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} gender={c.gender} birthday={c.birthday} job={c.job}/>)})}
+
         </TableBody>
         </Table>
+
+      
       </Paper>
     );
   }
